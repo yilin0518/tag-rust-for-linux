c8ee2bbab93d3e5c1224aa66cab01e45cdd2cb7a
yilin <21307130191@m.fudan.edu.cn>
2025-06-26 17:42:18 +0800

    Update
    Some Updates from upstream

--- include/uapi/linux/netfilter/xt_CONNMARK.h
+++ include/uapi/linux/netfilter/xt_CONNMARK.h
@@ -1,7 +1,9 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _XT_CONNMARK_H_target
-#define _XT_CONNMARK_H_target
+/* SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note */
+/* Copyright (C) 2002,2004 MARA Systems AB <https://www.marasystems.com>
+ * by Henrik Nordstrom <hno@marasystems.com>
+ */
 
-#include <linux/netfilter/xt_connmark.h>
+#ifndef _XT_CONNMARK_H
+#define _XT_CONNMARK_H
 
 #endif /*_XT_CONNMARK_H_target*/
